1974230e17f0f91b72cddca3394275d4f0399fe3
jin <jin@hyoo.ru>
2025-08-17 11:16:29 +0300

    layout

--- lines.view.css.ts
+++ lines.view.css.ts
@@ -22,36 +22,18 @@ namespace $.$$ {
 		},
 		
 		Board: {
+			margin: 'auto',
 			padding: '1vmin',
 			flex: {
 				direction: 'column',
 			},
-			align: {
-				self: 'stretch',
-				items: 'center',
-			},
-			justify: {
-				content: 'center',
-			},
 			userSelect: 'none',
-		},
-		
-		Row: {
-			// flexBasis: '9vmin',
-			// flex: {
-			// 	// basis: '10vmin',
-			// 	// grow: 1,
-			// },
 		},
 		
 		Cell: {
 			padding: '0.25vmin',
 			flex: {
 				basis: '9vmin',
-				// grow: 1,
-			},
-			background: {
-				color: $mol_theme.card,
 			},
 			aspectRatio: 1,
 			border: {
