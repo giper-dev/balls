bb2e504e0ddbeb6e1d665c04feec814fc55b92ea
jin <jin@hyoo.ru>
2026-02-04 21:02:55 +0300

    fix score

--- crowd/crowd.view.ts
+++ crowd/crowd.view.ts
@@ -62,7 +62,7 @@ namespace $.$$ {
 			const size = this.size()
 			const all = new Set< number >()
 			
-			const { score, score_max, kinds: init } = this.snapshot()
+			let { score, score_max, kinds: init } = this.snapshot()
 			const kinds = [ ... init ]
 			
 			const collect = ( row: number, col: number )=> {
@@ -84,7 +84,7 @@ namespace $.$$ {
 			collect( ... id )
 			
 			if( all.size < 2 ) return
-			this.score( this.score() + 2 ** all.size - 3 )
+			score += 2 ** all.size - 3
 			
 			for( const pos of all ) kinds[ pos ] = 0
 			this.snapshot({ score, score_max, kinds })
