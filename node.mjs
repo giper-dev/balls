1bc659915270a43f0843593d53b1a62f861d5a04
nin-jin <nin-jin@users.noreply.github.com>
2026-08-25 07:35:09 +0000

    Deploying to gh-pages from @ giper-dev/balls@0fd7e42f121e5b5021f5b6c145c86e9ef22be9c6 🚀

--- node.mjs
+++ node.mjs
@@ -5278,7 +5278,7 @@ var $;
          * `Infinity` - fulfilled
          */
         static level() {
-            return -Math.log2(1 - this.portion());
+            return Math.floor(-Math.log2(1 - this.portion()));
         }
     }
     $.$mol_storage = $mol_storage;
